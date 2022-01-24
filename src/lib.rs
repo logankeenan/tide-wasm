a0c2efda87d9fe003c24f25ff66a5bed4e4f1a8c
Logan Keenan <d.logankeenan@gmail.com>
2022-01-23 22:39:05 -0600

    add basic tide example

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use tide::http::{Url, Method, Request, Response};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -6,7 +7,21 @@ extern "C" {
     fn log(s: &str);
 }
 
+
+async fn hello_world(_req: tide::Request<()>) -> tide::Result {
+    let response = tide::Response::builder(200)
+        .body("Hello World")
+        .build();
+    Ok(response)
+}
+
 #[wasm_bindgen]
-pub fn request() {
-    log("hello world");
+pub async fn request() {
+    let mut app = tide::new();
+    app.at("/").get(hello_world);
+
+    let req = Request::new(Method::Get, Url::parse("http://localhost.com").unwrap());
+    let mut res: Response = app.respond(req).await.unwrap();
+    let body = res.body_string().await.unwrap();
+    log(body.as_str());
 }
